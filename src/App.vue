d0f585aecd0c947db42a64defc1730c2cd44f6c8
Thomas.N <pullagames@outlook.com>
2022-04-22 04:17:10 +0200

    :package: Add router
    Implement the vue-router package

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    {{ projectName }}
+    <nav>
+      <router-link to="/">
+        Home
+      </router-link> |
+      <router-link to="/about">
+        About
+      </router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
-<script>
-
-export default {
-  name: 'App',
-  components: {
-  },
-  data() {
-    return {
-      projectName: process.env.VUE_APP_PROJECT_NAME,
-    };
-  },
-};
-</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from '@/App.vue';
 
 import '@/assets/css/tailwind.css';
-import store from './store';
+import store from '@/store/index.js';
+import router from '@/router/index.js';
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: (h) => h(App),
 }).$mount('#app');
