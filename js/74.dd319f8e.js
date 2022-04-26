8d475265cb6fcc494eccfd9ee6e5e3a793a98b56
Thomas.N <pullagames@outlook.com>
2022-04-26 18:50:39 +0200

    deploying dist

---
+++ js/74.dd319f8e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkthe_ultimate_pokedex"]=self["webpackChunkthe_ultimate_pokedex"]||[]).push([[74],{74:function(e,t,u){u.r(t),u.d(t,{default:function(){return c}});var l=function(){var e=this,t=e.$createElement,u=e._self._c||t;return u("div",{staticClass:"about"},[e._v(" about ")])},n=[],a=u(1001),s={},r=(0,a.Z)(s,l,n,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=74.dd319f8e.js.map
